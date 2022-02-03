0fa7329022ed3078599a761c4e07cd8dd6a405f1
Orest27 <lokomo12272000@gmail.com>
2022-02-03 12:13:02 +0200

    update

---
+++ HW1/HW1.js
@@ -0,0 +1,83 @@
+let txt1 = 'hello';
+console.log(txt1);
+let txt2 = 'owu';
+console.log(txt2);
+let txt3 = 'com';
+console.log(txt3);
+let txt4 = 'ua';
+console.log(txt4);
+let number1 = '1';
+console.log(number1);
+let number2 = '10';
+console.log(number2);
+let number3 = '-999';
+console.log(number3);
+let number4 = '123';
+console.log(number4);
+let PI = '3.14';
+console.log(PI);
+let number5 = '2.7';
+console.log(number5);
+let number6 = '16';
+console.log(number6);
+let tr = 'true';
+console.log(tr);
+let fl = 'false';
+console.log(fl);
+let massive = [txt1, txt2, txt3, txt4, number1, number2, number3, number4, PI, number5, number6];
+alert(massive);
+document.write(massive); // завдання 1 створити змінні та вивести кожну змнінну
+txt1 = 'Revers';
+txt2 = 'jojo';
+txt3 = 'gOrdoN';
+txt4 = 'huba-buba';
+console.log( txt1, txt2, txt3, txt4)
+number1= '2';
+number2= '20';
+number3= '-111';
+number4= '321';
+PI= '3,14159265358979';
+number5= '4';
+number6= '3,3';
+console.log( number1, number2, number3, number4, PI, number5, number6 );
+let massive2 = [txt1, txt2, txt3, txt4, number1, number2, number3, number4, PI, number5, number6];
+alert(massive2);
+document.write(massive2); // завдання 2 переназвав кожну змінну таи вивів
+let firstName = 'Orest';
+let middleName = 'Voitovich';
+let lastName = 'Romanovuch';
+console.log(firstName, middleName, lastName)
+
+
+ let firstNameUser = prompt('Як тебе звати?')
+ let middleNameUser = prompt('Яке твоє прізвище?')
+ let ageUser = prompt('Який твій вік?')
+ let helloUses = ['Вітаю'+ ' '+ firstNameUser+ ' '+ middleNameUser+ '.'+ ' '+ 'Тобі'+ ' '+ ageUser+ '.' ]
+ alert(helloUses) // завдання 3 вивести за допомогою alert імя, прізвище і вік користувача
+ let aa = 100; 
+ let b = '100'; 
+ let c = true;
+ console.log(typeof aa); 
+ console.log(typeof b);
+ console.log(typeof c); // 4 завдання визначити тип, тип визначається після виконання 3 завдання
+console.log(5 < 6);
+console.log(5 > 6);
+console.log(5 === 6);
+console.log(5 == 6);
+console.log(10 == 10);
+console.log(10 === 10);
+console.log(10 > 10);
+console.log(10 !== 10);
+console.log(10 < 10);
+console.log(123 === '123');
+console.log(123 == '123'); // 5 завдання використати різні оператори для різних значень
+let str = "20";
+let a = 5;
+document.write(str + a + "<br/>");
+document.write(str - a + "<br/>");
+document.write(str * "2" + "<br/>");
+document.write(str / 2 + "<br/>")
+
+
+
+ 
