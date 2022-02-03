8bea0c213fd83b32449b44ebc93b870b05b49e69
Orest27 <lokomo12272000@gmail.com>
2022-02-03 22:28:29 +0200

    update 03.02.2022 22:28

---
+++ CW1/CW1.js
@@ -0,0 +1,43 @@
+let rn1 = Math.floor(Math.random()*100) + 1;
+let rn2 = Math.floor(Math.random()*100) + 1;
+let rn3 = Math.floor(Math.random()*100) + 1;
+let rn4 = Math.floor(Math.random()*100) + 1;
+let rn5 = Math.floor(Math.random()*100) + 1;
+let rn6 = Math.floor(Math.random()*100) + 1;
+let rn7 = Math.floor(Math.random()*100) + 1;
+let rn8 = Math.floor(Math.random()*100) + 1;
+let rn9 = Math.floor(Math.random()*100) + 1;
+let rn10 = Math.floor(Math.random()*100) + 1;
+let massive = [rn1, rn2, rn3, rn4, rn5, rn6, rn7, rn8, rn9, rn10,]
+console.log(massive)
+console.log(rn1 + rn2 + rn3 + rn4 + rn5 + rn6 + rn7 + rn8 + rn9 + rn10)
+let book = {
+    name: ['Harry Potter'],
+    pages: [673],
+    genre: ['Fantacy'],
+}
+console.log(book)
+let book2 = {
+    name: ['Harry Potter 2'],
+    pages: [706],
+    genre: ['Fantacy'],
+    authors: ['Joan Roaling'],
+}
+console.log(book2)
+let BOOK = [book, book2]
+console.log(BOOK)
+let height = [23]
+let widht = [10]
+let S = [widht*height]
+console.log(S)
+height = [10]
+let diameter = [4]
+let PI = [3.1415]
+let radius = [4/2]
+let V = [PI*(radius*radius)*height]
+console.log(V)
+let katet1 = [3]
+let katet2 = [4]
+let hypotenuse = [Math.sqrt(Math.pow(katet1,2)+Math.pow(katet2,2))]
+console.log(hypotenuse)
+
