e052bb5d6e010e9655ce723797a61cd5b000adab
Orest27 <lokomo12272000@gmail.com>
2023-01-05 09:04:50 +0200

    new

---
+++ 2023(1)/2023jstask.js
@@ -0,0 +1,18 @@
+let hello = 'hello';
+let owu = 'owu';
+let com = 'com';
+let ua = 'ua';
+console.log(hello + " " + owu + '.' + com + ua)
+let firstName = 'Orest';
+let middleName = 'Romanovuch';
+let lastName = 'Voitovych';
+console.log(firstName + " " + middleName + ' ' + lastName)
+let a = 100; 
+let b = '100'; 
+let c = true;
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+function fun() {  
+    prompt ("This is a prompt box with my name and age", "Orest 22");  
+    } 
