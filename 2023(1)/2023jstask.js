d1502040eae82d0f78dce5e4f37255635d0cabd1
Orest27 <lokomo12272000@gmail.com>
2023-01-11 13:24:44 +0200

    update

--- 2023(1)/2023jstask.js
+++ 2023(1)/2023jstask.js
@@ -13,6 +13,16 @@ let c = true;
 console.log(typeof a)
 console.log(typeof b)
 console.log(typeof c)
-function fun() {  
-    prompt ("This is a prompt box with my name and age", "Orest 22");  
-    } 
+function foo() {
+    var userAnswer = prompt("What is your name?");
+    return userAnswer;
+}
+function foo1() {
+    var userAnswer = prompt("What is your middlename?");
+    return userAnswer;
+}
+function foo2() {
+    var userAnswer = prompt("What is your lastname?");
+    return userAnswer;
+}
+console.log(foo() + " " + foo1() + " " + foo2());
