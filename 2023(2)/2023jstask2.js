06e7bacefcac010a27ccfcfcbde2c9b9be2e09a1
Orest27 <lokomo12272000@gmail.com>
2023-01-11 21:26:42 +0200

    Update

---
+++ 2023(2)/2023jstask2.js
@@ -0,0 +1,38 @@
+let domen = ['Jacope', 12, true,  false, 'kum', 'pam', 15, 'georgii', 55, 'ferum'];
+let book = {
+    title : 'Bookers',
+    pageCount : 565,
+    genre : 'horror'
+}
+let book2 = {
+    title : 'BoomBeach',
+    pageCount : 1125,
+    genre : 'horror',
+    autors : [
+        {name: 'Alex', age: 33}, 
+        {name: 'Oleg', age: 47},
+        {name: 'Igor', age: 27},
+    ],
+};
+ let users = [
+    {name: 'Jonch', username: 'ERacl', password: 'asdf123'},
+    {name: 'Igor', username: 'lokomo', password: '99cghk'},
+    {name: 'Alex', username: 'hippop', password: 'axcg2f3'},
+    {name: 'Olha', username: 'SuNshine', password: 'fik7h654g'},
+    {name: 'Jerca', username: 'JustFUN', password: '7895jhdfh54'},
+    {name: 'Kolin', username: 'HyperL', password: 'cnj34b6'},
+    {name: 'Marcus', username: 'Catolik', password: 'lo54rf3ed'},
+    {name: 'Hendelf', username: 'massivboy', password: 'qwerty321'},
+    {name: 'Dima', username: 'psyho', password: 'ku7899fgdf5'},
+    {name: 'Roma', username: 'Romanovuch', password: 'auuu5t3rf'},
+ ];
+ console.log(users[0].password);
+ console.log(users[1].password);
+ console.log(users[2].password);
+ console.log(users[3].password);
+ console.log(users[4].password);
+ console.log(users[5].password);
+ console.log(users[6].password);
+ console.log(users[7].password);
+ console.log(users[8].password);
+ console.log(users[9].password);
